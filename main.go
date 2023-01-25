1520655faeaffd1ccb832aee51520266ef4a5263
Ruslan_L <rus059111@gmailcom>
2023-01-25 12:54:40 +0600

    finish

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+//Задание 2. Шахматная доска
+
+import (
+	"fmt"
+)
+
+func main() {
+	var width int
+	var height int
+	fmt.Println("Введите ширину:")
+	fmt.Scan(&width)
+	fmt.Println("Введите высоту:")
+	fmt.Scan(&height)
+
+	count := 4
+
+	for i := 0; i < count; i++ {
+		fmt.Println("*")
+	}
+}
